388fb485f3fcd143e4b25f8acee0c727e66f45bb
rbassell <rbassell@bates.edu>
2014-05-05 19:31:10 -0400

    changes in config/environments/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,16 @@ Pinterestforpuppies::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  #tried to change this to true - originally was false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #tried to change this to true - originally was false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
---
+++ test/helpers/costumes_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class CostumesHelperTest < ActionView::TestCase
+end
